84cd8a21a1ac6b5ab5f40d8bd0bb93f3f32c27ba
EliverLara <eliverlara@gmail.com>
2018-02-14 16:31:32 -0600

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var exec = require('gulp-exec');
+
+gulp.task('styles', function() {
+    gulp.src('gtk-3.20/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./gtk-3.20/'))
+        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Snow"'))
+});
+
+gulp.task('shell-style', function() {
+    gulp.src('gnome-shell/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./gnome-shell/'))
+        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Snow"'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('gtk-3.20/**/*.scss',['styles']);
+});
+
+gulp.task('shell',function() {
+    gulp.watch('gnome-shell/*.scss',['shell-style']);
+});
