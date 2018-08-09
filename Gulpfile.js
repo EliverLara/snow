fce292bf0b5c1525100f6473962da866886859f0
EliverLara <eliverlara@gmail.com>
2018-08-08 19:19:13 -0500

    Remove popover shadow on dark theme

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,25 +2,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var exec = require('gulp-exec');
 
-gulp.task('styles', function() {
+gulp.task('styles', function(done) {
     gulp.src('gtk-3.0/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gtk-3.0/'))
         .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Snow"'))
+    done();
 });
 
-gulp.task('shell-style', function() {
+gulp.task('shell-style', function(done) {
     gulp.src('gnome-shell/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gnome-shell/'))
         .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Snow"'))
+    done();
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('gtk-3.0/**/*.scss',['styles']);
+    gulp.watch('gtk-3.0/**/*.scss', gulp.series('styles'));
 });
 
 gulp.task('shell',function() {
-    gulp.watch('gnome-shell/*.scss',['shell-style']);
+    gulp.watch('gnome-shell/*.scss', gulp.series('shell-style'));
 });
